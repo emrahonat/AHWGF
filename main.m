8816081abb29e3444f988ae75b0c561d4c775f2b
emrah onat <eonat87@yahoo.com>
2025-09-29 00:04:43 +0200

    first commit

---
+++ main.m
@@ -0,0 +1,9 @@
+%% MATLAB Code for Adaptive Hybrid Wavelet-Goldstein Filter 
+%
+% AHWGF, as an alternative interferogram filtering algorithm
+% 
+%
+% SAR Interferometry Applications
+%
+% Dr. Emrah Onat
+% 28.09.2025
